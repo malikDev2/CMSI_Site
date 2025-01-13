46b79acf21988a29fd18cf3245059a53f19f50e1
Malik <malik.h.jacobs@outlook.com>
2025-01-13 13:32:41 -0500

    started theory cards

--- src/theories.jsx
+++ src/theories.jsx
@@ -1,9 +1,31 @@
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import './App.css';
+import Card2 from './card2.jsx';
+import images from './images.jsx';
+
+
 function Theories(){
+    // useState to change selected theory
+    const [ selectedTheory, setSelectedTheory] = useState('');
+
+    const handleSelectChange = (e) => {
+    setSelectedTheory(e.target.value);
+    }
+
     return(<>
+    <div className="theories">
+        <select name="theo" id="theorySelector" className="selector" onChange={handleSelectChange}>
+            <option value="">Select a theory</option>
+            <option value="Util">Utilitarianism</option>
+            <option value="Raw">Rawls theory of justice</option>
+            <option value="Norm">Normative Theories</option>
+            <option value="SCT">Social Contract Theory</option>
+        </select>
     <Link to='/' className='homeButton'>
         <h1>Home</h1>
     </Link>
+    </div>
         </>);
 }
 
